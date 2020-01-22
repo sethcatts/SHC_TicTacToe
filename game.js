3098fb8e9b40d2b653b17fc1352df5a265393199
Seth Catts <11430259+sethcatts@users.noreply.github.com>
2020-01-22 16:36:31 -0500

    Update game.js
    Adding basic class things

--- game.js
+++ game.js
@@ -1,5 +1,25 @@
-//Game class
+class Game {
+  var board = [[],[]];
+  /*
+  * ARGS: number to be squared to create board of size n
+  */
+  constructor(3) {
+     createBoard(board);
+  }
 
-class game() {
+  function createBoard(size, board) {
+    for(let i = 0; i < size; i++) {
+      for(let j = 0; j < size; j++) {
+          board[i][j] = x;
+      }
+    }
+    return board;
+  }
   
+  function getBoard() {
+    return this.board;
+  }
+
 }
+const game_1 = new Game(3);
+console.error(game.getBoard());
