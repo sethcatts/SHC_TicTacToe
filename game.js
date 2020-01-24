853cdbf54567e370c37bab0cc1a74dc827d3c39b
Seth <seth.h.catts@gmail.com>
2020-01-23 23:02:21 -0500

    Adding basic board creation and printing

--- game.js
+++ game.js
@@ -1,31 +1,47 @@
 class Game {
-  var board = [[],[]];
-  var win   = false;
-  var player_1 = new Player("One");
-  var player_2 = new Player("Two");
   /*
   * ARGS: number to be squared to create board of size n
   */
-  constructor(3) {
-     createBoard(board);
+  constructor(boardSize) {
+    var board;
+    var gameOver;
+    this.board      = this.createBoard(boardSize);
+    this.gameOver   = false;
+    //this.player_1   = new Player("One");
+    //this.player_2   = new Player("Two");
   }
 
-  function createBoard(size, board) {
+  createBoard(size) {
+    let board = [];
     for(let i = 0; i < size; i++) {
+      let tempArray = [];
       for(let j = 0; j < size; j++) {
-          board[i][j] = x;
+          tempArray.push("X");
       }
+      board.push(tempArray);
+      tempArray = [];
     }
     return board;
   }
   
-  function getBoard() {
+  getBoard() {
     return this.board;
   }
   
-  function checkForWin() {
+  setBoard(x) {
+    this.board = x;
+  }
+
+  prtBoard() {
+    for(let x in this.board) {
+      console.error(this.board[x]);
+    }
+  }
+  
+  checkForWin() {
     return false;
   }
 }
+
 const game_1 = new Game(3);
-console.error(game.getBoard());
+console.error(game_1.prtBoard());
