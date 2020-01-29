dbca0fd6f9488538dc650561bc14c018f852eb95
Seth <seth.h.catts@gmail.com>
2020-01-28 20:59:46 -0500

    Updating testing code

--- instance.js
+++ instance.js
@@ -10,9 +10,11 @@ const game_1 = new Game(3);
 
 game_1.prtBoard();
 readline.question('Command: ', (com) => {
-    console.error(com);
-    input = com; 
+    com = com.split("");
+    game_1.placePiece("X", com[0], com[1]) 
     readline.close();
+    game_1.prtBoard();
 });
+
 
 
