458e9932f35aebd1666f187ff9bc58981a128b0a
Seth Catts <scatts@monticello.org>
2020-02-03 13:14:57 -0500

    Creating AI function file

---
+++ ai_player.js
@@ -0,0 +1,9 @@
+/*
+**  Function: Return the coordinates of a move
+**            based on the board and difficulty 
+**  @args: Current gameboard, piece placement level
+**  @returns: Placement coordinates
+*/
+function getMove(board, difficulty) {
+    //TODO
+}
