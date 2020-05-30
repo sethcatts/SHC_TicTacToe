f6355a39a9e80c5bdc9c177fd62d8113b474d55b
Seth <seth.h.catts@gmail.com>
2020-05-30 18:08:46 -0400

    Starting AI class

--- ai_player.js
+++ ai_player.js
@@ -1,9 +1,29 @@
-/*
-**  Function: Return the coordinates of a move
-**            based on the board and difficulty 
-**  @args: Current gameboard, piece placement level
-**  @returns: Placement coordinates
-*/
-function getMove(board, difficulty) {
-    //TODO
+//https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
+
+class AI_Player extends Player {
+    constructor(name, piece, color, difficulty) {
+        super(name, piece, color);
+        this.difficulty = difficulty;
+    }
+
+    getBestMove(board) {
+        //TODO
+    }
+
+    minimax(board, depth, isMaximazingPlayer) {
+        if(/*current board state is terminal*/) {
+            //return value of board
+        }
+
+        if(isMaximazingPlayer) {
+            var bestVal = -Infinity;
+            //for()
+        }
+    }
+
+    isMovesLeft(board) {
+        //TODO
+    }
+
+
 }
