31660cc6cf91fcc68b924129ad432b486203efda
Seth <seth.h.catts@gmail.com>
2020-05-30 21:03:20 -0400

    Refactoring

--- interface.js
+++ interface.js
@@ -1,29 +1,6 @@
 //Create Game Object
 var game_instance = new Game();
 
-/*
-function selectCell_x(idn) {
-    var cell = document.getElementById(idn[0] + "" + idn[1]);
-    var elem = document.createElement('img');
-    elem.className = "selected_cell";
-    if(game_instance.currentPlayer == game_instance.player_1 && !game_instance.gameOver && game_instance.legalMove(idn[0], idn[1])) {
-        game_instance.placePiece(idn[0], idn[1])
-        elem.src = game_instance.player_1.getPieceImage();
-        cell.appendChild(elem);     
-    } else if (!game_instance.gameOver){
-        elem.src = game_instance.player_2.getPieceImage();
-        cell.appendChild(elem);
-        game_instance.placePiece(idn[0], idn[1]);
-    } 
-    if(game_instance.checkForWin()) {
-        alert("Game Over! \n" + game_instance.waitingPlayer.getName() + " Wins!");
-    }
-    if(game_instance.checkForTie()) {
-        alert("Game Over! \n Tie!");
-    }
-}
-*/
-
 function selectCell(idn) {
     var cell = document.getElementById(idn[0] + "" + idn[1]);
     var elem = document.createElement('img');
