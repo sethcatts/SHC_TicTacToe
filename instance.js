c256afd9e43e37b4516f5f5882fe3ff54f3bf301
Seth <seth.h.catts@gmail.com>
2020-06-23 11:48:34 -0400

    ai moves

--- instance.js
+++ instance.js
@@ -10,14 +10,23 @@ function selectCell(idn) {
     var cell = document.getElementById(idn[0] + "" + idn[1]);
     var elem = document.createElement('img');
     elem.className = "selected_cell";
-
-
-    //How to allow the GUI to reflect AI moves. Kinda hard
     if(!game_instance.gameOver && game_instance.legalMove(idn[0], idn[1])) {
         game_instance.placePiece(idn[0],idn[1]);
         elem.src = game_instance.currentPlayer.getPieceImage();
         cell.appendChild(elem);
     } 
+    if(game_instance.checkForWin()) {
+        alert("Game Over! \n" + game_instance.currentPlayer.getName() + " Wins!");
+    }
+    if(game_instance.checkForTie()) {
+        alert("Game Over! \n" + "The result is a tie!");
+    }
+
+    if(ai_enabled && game_instance.currentPlayer != game_instance.player_2) {
+        //Make ai move
+    }
+
+    //COPY PASTE -- Make one function?
     if(game_instance.checkForWin()) {
         alert("Game Over! \n" + game_instance.currentPlayer.getName() + " Wins!");
     }
