4cc2fa20b6b893479aa0c87abecf6bc7ec249101
Seth <seth.h.catts@gmail.com>
2020-08-26 20:36:50 -0400

    Finishing tests

--- ai_test.js
+++ ai_test.js
@@ -4,20 +4,9 @@ function dpboard(board) {
     console.log(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
     console.log(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
     console.log(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+    console.log("-");
 }
-/* ============================ AI TESTING ================================ *
-MEM: ► (9744), • (7), ○ (9), 
-    >> TODO <<
-    ► Check AI class methods
-        • getBestMove
-        • evaluate
-        ○ minmax
-        • isMovesLeft
-
-        TODO:   the algo is never getting a better score than the ones that 
-                it initially gets. see line 47 for the reason that it's now 
-                always returning the last move.
- * ======================================================================== */
+/* ============================ AI TESTING ================================ */
 //Best move boards
 var board1 = [["o","o","x"],
               ["-","-","-"],
@@ -30,10 +19,10 @@ var board3 = [["o","x","x"],
               ["o","o","x"]];
 /*-- Eval boards
 var board_x = [["x","x","x"],
-              ["-","-","o"],
+              ["-","o","o"],
               ["o","-","x"]];  
 var board_y = [["x","x","o"],
-              ["-","-","o"],
+              ["x","-","o"],
               ["o","-","o"]]; 
 var board_z = [["x","o","x"],
               ["o","-","x"],
@@ -41,22 +30,25 @@ var board_z = [["x","o","x"],
 var board_n = [["x","o","x"],
               ["-","-","x"],
               ["o","o","o"]];      
-console.log("board eval: " + ai.evaluate(board1));
-console.log("board eval: " + ai.evaluate(board2));
-console.log("board eval: " + ai.evaluate(board3));
+console.log("board eval: " + ai.evaluate(board_x));
+console.log("board eval: " + ai.evaluate(board_y));
+console.log("board eval: " + ai.evaluate(board_z));
+console.log("board eval: " + ai.evaluate(board_n));
 */
+//console.log("Eval: " + ai.evaluate(board1));
+
 //console.log("Best move for board 1 is: " + ai.getBestMove(board1));
 var b1bm = ai.getBestMove(board1);
-board1[b1bm[0]][b1bm[1]] = "P";
+board1[b1bm[0]][b1bm[1]] = "+";
 //console.log(board1);
 dpboard(board1);
 //console.log("Best move for board 2 is: " + ai.getBestMove(board2));
 var b2bm = ai.getBestMove(board2);
-board2[b2bm[0]][b2bm[1]] = "P";
+board2[b2bm[0]][b2bm[1]] = "+";
 //console.log(board2);
 dpboard(board2);
 //console.log("Best move for board 3 is: " + ai.getBestMove(board3));
 var b3bm = ai.getBestMove(board3);
-board3[b3bm[0]][b3bm[1]] = "P";
+board3[b3bm[0]][b3bm[1]] = "+";
 //console.log(board3);
 dpboard(board3);
