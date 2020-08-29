394c67a2fa8f6a023fbe16bfafceb78cad03f16a
Seth <seth.h.catts@gmail.com>
2020-08-28 22:59:32 -0400

    ai testing

--- ai_test.js
+++ ai_test.js
@@ -7,10 +7,11 @@ function dpboard(board) {
     console.log("-");
 }
 /* ============================ AI TESTING ================================ */
-/*Best move boards
-var board1 = [["o","o","x"],
-              ["-","-","-"],
-              ["o","x","x"]];
+//Best move boards
+var board1 = [["x","-","-"],
+              ["-","o","o"],
+              ["-","-","-"]];
+/*
 var board2 = [["x","o","x"],
               ["x","-","o"],
               ["-","o","-"]];
@@ -18,14 +19,14 @@ var board3 = [["o","x","x"],
               ["-","-","o"],
               ["o","o","x"]];
 */
-//-- Eval boards
-var board_x = [["x","-","-"],
-               ["-","x","-"],
-               ["-","-","x"]]; 
+/*-- Eval boards
+var board_x = [["-","-","-"],
+               ["-","-","-"],
+               ["-","-","-"]]; 
 
 var board_y = [["-","-","-"],
                ["-","-","-"],
-               ["x","x","x"]]; 
+               ["-","-","-"]]; 
 
 var board_z = [["-","-","-"],
                ["-","-","-"],
@@ -34,17 +35,20 @@ var board_z = [["-","-","-"],
 var board_n = [["-","-","-"],
                ["-","-","-"],
                ["-","-","-"]];      
-console.log(" 10 | board eval: " + ai.evaluate(board_x));
+//console.log(" 10 | board eval: " + ai.evaluate(board_x));
 //console.log(" 10 | board eval: " + ai.evaluate(board_y));
 //console.log(" xx | board eval: " + ai.evaluate(board_z));
 //console.log(" xx | board eval: " + ai.evaluate(board_n));
 //console.log("Eval: " + ai.evaluate(board1));
 
 //console.log("Best move for board 1 is: " + ai.getBestMove(board1));
+*/
+
 var b1bm = ai.getBestMove(board1);
 board1[b1bm[0]][b1bm[1]] = "+";
 //console.log(board1);
 dpboard(board1);
+/*
 //console.log("Best move for board 2 is: " + ai.getBestMove(board2));
 var b2bm = ai.getBestMove(board2);
 board2[b2bm[0]][b2bm[1]] = "+";
@@ -55,3 +59,4 @@ var b3bm = ai.getBestMove(board3);
 board3[b3bm[0]][b3bm[1]] = "+";
 //console.log(board3);
 dpboard(board3);
+*/
